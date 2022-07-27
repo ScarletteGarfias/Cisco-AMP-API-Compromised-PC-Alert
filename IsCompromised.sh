5aeb8b313872fbde0e9001f6f00e8867a0dd5d4a
ScarletteGarfias <103275061+ScarletteGarfias@users.noreply.github.com>
2022-07-27 13:43:30 -0500

    Update IsCompromised.sh

--- IsCompromised.sh
+++ IsCompromised.sh
@@ -2,27 +2,41 @@
 
 CompromisedWC=$(cat /home/Compromised.txt | wc -l)
 CompromisedUniq=$(cat /home/Compromised.txt | sort | uniq)
+CurlWC=$(curl 'https://api.amp.cisco.com/v1/computers?group_guid=GROUPID' -u 'ClientID:Key' | jq \
+| grep 'external\|hostname\|is_compromised' | paste - - - | column -t -s$'\t' | grep true | sort | uniq | wc -l )
 
-if [[ $CompromisedWC -eq "0" ]]; then
+if [[ $CompromisedWC -eq 0 ]] && [[ $CurlWC -gt 0 ]]; then
+
 cp /home/Sendmail.txt /home/Sendmailtemp.txt
-curl 'https://api.amp.cisco.com/v1/computers' -u 'ClientID:KEY' | jq | grep 'external\|hostname\|is_compromised' \
-| paste - - - | column -t -s$'\t' | grep true >> /home/Compromised.txt
+echo "Subject: Indication of Compromise" >> /home/Sendmailtemp.txt
+echo >> /home/Sendmailtemp.txt
+
+curl 'https://api.amp.cisco.com/v1/computers?group_guid=GROUPID' -u 'ClientID:Key' | jq \
+| grep 'external\|hostname\|is_compromised' | paste - - - | column -t -s$'\t' | grep true >> /home/Compromised.txt
 cat /home/Compromised.txt >> /home/Sendmailtemp.txt
 sudo cat /home/Sendmailtemp.txt | ssmtp user@email.com
 rm /home/Sendmailtemp.txt
+
 else
+CurlAPI=$(curl 'https://api.amp.cisco.com/v1/computers?group_guid=GROUPID' -u 'ClientID:Key' | jq \
+| grep 'external\|hostname\|is_compromised' | paste - - - | column -t -s$'\t' | grep true | sort | uniq )
 
-Compromised=$(curl 'https://api.amp.cisco.com/v1/computers' -u 'ClientID:KEY' | jq | grep 'external\|hostname\|is_compromised' \
-| paste - - - | column -t -s$'\t' | grep true | sort | uniq )
-
-if [[ "$Compromised" != "$CompromisedUniq" ]]; then
+if [[ "$CompromisedUniq" != "$CurlAPI" ]]; then
 
 cp /home/Sendmail.txt /home/Sendmailtemp.txt
-curl 'https://api.amp.cisco.com/v1/computers' -u 'ClientID:Key' | jq | grep 'external\|hostname\|is_compromised' \
-| paste - - - | column -t -s$'\t' | grep true >> /home/Compromised.txt
+echo "Subject: Indication of Compromise" >> /home/Sendmailtemp.txt
+echo >> /home/Sendmailtemp.txt
+
+curl 'https://api.amp.cisco.com/v1/computers?group_guid=GROUPID' -u 'ClientID:Key' | jq \
+| grep 'external\|hostname\|is_compromised' | paste - - - | column -t -s$'\t' | grep true >> /home/Compromised.txt
 cat /home/Compromised.txt | sort | uniq >> /home/Sendmailtemp.txt
 sudo cat /home/Sendmailtemp.txt | ssmtp user@email.com
 rm /home/Sendmailtemp.txt
 
 fi
 fi
+
+
+
+
+
